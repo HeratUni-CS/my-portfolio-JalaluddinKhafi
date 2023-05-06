d169e2ebcc1c4cce9e32296cd4f87a76532a8f2e
Jalaluddin Khafi <Khafi98117@gmail.com>
2023-05-06 22:33:50 +0430

    First card added to project

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,49 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         body: SafeArea(
-          child:Column(
-            children: const [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/Khafi.jpg'),
-              ),
-              Text(
-                'Jalaluddin Khafi',
-                style: TextStyle(
-                  color:Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/Khafi.jpg'),
                 ),
-              )
-            ],
-          )   )
+                Text(
+                  'Jalaluddin Khafi',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Student',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 2,
+                  ),
+                ),
+                Card(
+                  margin:EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                  child: ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text(
+                        '+093729715150',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      );
+      ),
+    );
   }
 }
